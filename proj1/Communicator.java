1fa36f1d9763ab0355380e0cfc9395b74878597b
igonzalez39 <73077497+igonzalez39@users.noreply.github.com>
2020-10-21 13:00:17 -0700

    Update Communicator.java

--- proj1/Communicator.java
+++ proj1/Communicator.java
@@ -1,36 +1,59 @@
+import nachos;
 package nachos;
 
 public class Communicator {
 
-	public Communicator(){
-	//implement mutex function
-	//here we implement a listen function and a speaking function 
-	ListenFunc. //set this equal to the mutex func.
-	SpeakFunc. //do the same to this one also
+	public Communicator() {
+		mutex = new Lock();		//implement mutex function
+	
+		//here we implement a listen function and a speaking function 
+		LReadyUp = new Condition(mutex);		
+		SReadyUp = new Condition(mutex); 		
 	}
+	
 	//we then have to be able to create 2 public void functions for speaking and listening
 	//we also have to have the thread listen via the communicator.
 	//from there it should transfer the message to ListingFunc.
 	//so bascally the thread to be paired with one of the ListingFunc. thread.
-	public void  SpeakFunc. (message){
-	//add mutex function
-	//initialize SpeakingFunc. And iterate twice ++
-	while(ListingFunc doesn’t equal anything){
-	//put to sleep the ListenFunc. 
+	public void  SpeakFunc(int message){
+		mutex.acquire();
+		SpeakingFunc++;
+		
+		//put to sleep the ListenFunc. 
+		while(ListingFunc = 0){
+			LReadyUp.sleep();
+		}
+		
+		--ListeningFunc;
+		
+		//once thatâ€™s done we have to wake the SpeakFunc.
+		SReadyUp.wake();
+		mutex.release();
 	}
-	//de-iterate the ListeningFunc. –
-	//once that’s done we have to make the SpeakFunc.
-	//add mutex
-	}
+	
 	//add the public void function for listening
-	public void ListenFunc. (){
-	//add mutex function
-	//wake up the ListenFunc. Then iterate it
-	while(SpeakingFunc. doesn’t equal anyth{
-	//put the SpeakFunc. to sleep
+	public int ListenFunc (){
+		mutex.acquire();
+		ListeningFunc++;
+		
+		LReadyUp.wake();
+		
+		while(SpeakingFunc = 0){
+			SReadyUp.sleep();
+		}
+		
+		--SpeakingFunc;
+		
+		mutex.release();
+			
+		//I think we have to return the message at this point
+		return message;
 	}
-	//de-iterate the SpeakingFunc. –
-	//I think we have to return the message at this point
-	}
+	private Condition LReadyUp;
+	private Condition SReadyUp;
+	private int SpeakingFunc = 0;
+	private int ListeningFunc = 0;
+	private int messenger;
+	private Lock mutex;
 	}
 
